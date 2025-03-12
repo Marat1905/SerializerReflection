d73a5c741cd9df7b7d551080a38b4af57a623b7d
Marat <milya010790@mail.ru>
2025-03-12 12:03:07 +0500

    Расчет сериализации тестового класса

--- SerializerReflection/Program.cs
+++ SerializerReflection/Program.cs
@@ -1,4 +1,7 @@
 ﻿using SerializerReflection.Classes;
+using SerializerReflection.Extensions;
+using System.Reflection.Emit;
+using System.Text.Json;
 
 namespace SerializerReflection
 {
@@ -6,9 +9,21 @@ namespace SerializerReflection
     {
         static void Main(string[] args)
         {
+            List<int> collectionSizes = [100_000, 1_000_000, 10_000_000];
+            PrintInfoPC.PrintSystemInfo();
+
             var testClass = new F().Get();
 
             Console.WriteLine(SerializationToString.Serialize(testClass));
+
+            foreach (int i in collectionSizes)
+            {
+                Logger.WriteTitle($"_______________Расчет сериализации  {i.ToString("0,0")} раз_______________");
+
+                TimingControl<string>.MeasureTime(() => SerializationToString.Serialize(testClass),i, "Расчет сериализации с помощью Reflection");
+
+                TimingControl<string>.MeasureTime(() => JsonSerializer.Serialize(testClass), i, "Расчет сериализации с помощью JsonSerializer");
+            }
         }
     }
 }
