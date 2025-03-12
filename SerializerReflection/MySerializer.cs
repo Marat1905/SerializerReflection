c2b8817eca7ca67c29022044931eec300ba88998
Marat <milya010790@mail.ru>
2025-03-12 11:13:26 +0500

    Добавьте файлы проекта.

---
+++ SerializerReflection/MySerializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace SerializerReflection
+{
+    public class MySerializer
+    {
+        public static string Serialize<T>(T obj)
+        {
+            var fields = obj.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+            return string.Format("{{{0}}}", string.Join(" ", fields.Select(x => string.Format("{0} = {1};", x.Name, x.GetValue(obj)))));
+        }
+    }
+}
---
+++ SerializerReflection/Program.cs
@@ -0,0 +1,14 @@
+﻿using SerializerReflection.Classes;
+
+namespace SerializerReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var testClass = new F().Get();
+
+            Console.WriteLine(MySerializer.Serialize(testClass));
+        }
+    }
+}
